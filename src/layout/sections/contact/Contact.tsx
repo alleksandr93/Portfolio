12e8c6c35c65f6f51530a07257a221637bc12500
Aleksandr <aleksandr-ishutin93@mail.ru>
2025-03-26 06:33:19 +0400

    change form3

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -2,51 +2,44 @@ import {SectionTitle} from '../../../components/SectionTitle.ts';
 import {Button} from '../../../components/Button.ts';
 import {Container} from '../../../components/Container.ts';
 import {S} from './Contact_Styles.ts'
-import {type MouseEvent, useState} from 'react';
+import {type MouseEvent, useRef} from 'react';
 import axios from 'axios';
 
-const vercel ='https://email-sendler.vercel.app/send'
+const vercel = 'https://email-sendler.vercel.app/send'
 // const host = 'http://localhost:3010/send'
 export const Contact = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [company, setCompany] = useState('');
-    const [contact, setContact] = useState('');
-    const [text, setText] = useState('');
+    const name = useRef<HTMLInputElement>(null);
+    const email = useRef<HTMLInputElement>(null);
+    const company = useRef<HTMLInputElement>(null);
+    const contact = useRef<HTMLInputElement>(null);
+    const text = useRef<HTMLTextAreaElement>(null);
 
-    const onClickHandler =  (e: MouseEvent<HTMLButtonElement>) => {
+    const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-      axios.post(vercel, {
+        axios.post(vercel, {
             username: name,
             customMessage: text,
             company,
             contact,
             email
-        }).then(res=>{
-          if(res.data.success===true){
-              setName('')
-              setEmail('')
-              setContact('')
-              setCompany('')
-              setText('')
-          }
-      })
+        }).then(() => {
+            if(name.current) name.current.value = '';
+            if(email.current) email.current.value = '';
+            if(company.current) company.current.value = '';
+            if(contact.current) contact.current.value = '';
+            if(text.current) text.current.value = '';
+        })
     }
     return (
         <S.Contact id="contact">
             <Container>
                 <SectionTitle>Contact</SectionTitle>
                 <S.Form>
-                    <S.Field value={name} name={'name'} placeholder={'Name'}
-                             onChange={(e) => setName(e.currentTarget.value)}/>
-                    <S.Field value={email} name={'email'} placeholder={'email'}
-                             onChange={(e) => setEmail(e.currentTarget.value)}/>
-                    <S.Field value={company} name={'company'} placeholder={'company'}
-                             onChange={(e) => setCompany(e.currentTarget.value)}/>
-                    <S.Field value={contact} name={'contact'} placeholder={'contact'}
-                             onChange={(e) => setContact(e.currentTarget.value)}/>
-                    <S.Field value={text} placeholder={'message'} as={'textarea'}
-                             onChange={(e) => setText(e.currentTarget.value)}/>
+                    <S.Field ref={name} name={'name'} placeholder={'Name'}/>
+                    <S.Field ref={email} name={'email'} placeholder={'email'}/>
+                    <S.Field ref={company} name={'company'} placeholder={'company'}/>
+                    <S.Field ref={contact} name={'contact'} placeholder={'contact'}/>
+                    <S.Field ref={text} placeholder={'message'} as={'textarea'}/>
                     <Button onClick={onClickHandler} type={'submit'}>Send message</Button>
                 </S.Form>
             </Container>
