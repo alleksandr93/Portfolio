597e55d4818a029052d220fe9dad279c338999b1
Aleksandr <aleksandr-ishutin93@mail.ru>
2025-03-26 05:14:19 +0400

    send mail

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -2,18 +2,42 @@ import {SectionTitle} from '../../../components/SectionTitle.ts';
 import {Button} from '../../../components/Button.ts';
 import {Container} from '../../../components/Container.ts';
 import {S} from './Contact_Styles.ts'
+import {type MouseEvent, useState} from 'react';
+import axios from 'axios';
 
 
 export const Contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [company, setCompany] = useState('');
+    const [contact, setContact] = useState('');
+    const [text, setText] = useState('');
+    const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        axios.post('https://email-sendler.vercel.app/send', {
+            username: name,
+            customMessage: text,
+            company,
+            contact,
+            email
+        }).then(res => console.log(res))
+    }
     return (
         <S.Contact id="contact">
             <Container>
                 <SectionTitle>Contact</SectionTitle>
                 <S.Form>
-                    <S.Field name={'name'} placeholder={'Name'}/>
-                    <S.Field name={'lastName'} placeholder={'Subject'}/>
-                    <S.Field placeholder={'message'} as={'textarea'}/>
-                    <Button type={'submit'}>Send message</Button>
+                    <S.Field value={name} name={'name'} placeholder={'Name'}
+                             onChange={(e) => setName(e.currentTarget.value)}/>
+                    <S.Field value={email} name={'email'} placeholder={'email'}
+                             onChange={(e) => setEmail(e.currentTarget.value)}/>
+                    <S.Field value={company} name={'company'} placeholder={'company'}
+                             onChange={(e) => setCompany(e.currentTarget.value)}/>
+                    <S.Field value={contact} name={'contact'} placeholder={'contact'}
+                             onChange={(e) => setContact(e.currentTarget.value)}/>
+                    <S.Field value={text} placeholder={'message'} as={'textarea'}
+                             onChange={(e) => setText(e.currentTarget.value)}/>
+                    <Button onClick={onClickHandler} type={'submit'}>Send message</Button>
                 </S.Form>
             </Container>
         </S.Contact>
